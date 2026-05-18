1f8f33ecc01b14815c8bf27bc04cf63b1c13394e
20035888-sam <20035888@students.koi.edu.au>
2026-05-18 22:13:51 +1000

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,14 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+const schema = a.schema({
+  Todo: a
+    .model({
+      content: a.string().required(),
+      completed: a.boolean().default(false),
+      priority: a.string().default("medium"),
+    })
+    .authorization((allow) => [allow.owner()]),
 });
+
+export type Schema = typeof schema;
+export default defineData({ schema });
